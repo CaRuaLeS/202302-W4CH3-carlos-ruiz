a1f6bcf369c3c5f7a0f5fab4943a4c92d46b7507
CarlosRuiz <carua97@gmail.com>
2023-02-08 18:03:07 +0100

    Reset proyect copy

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { UserForm } from "../user-form/user-form";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <p>Formulario</p>
-      <UserForm></UserForm>
     </div>
   );
 }
