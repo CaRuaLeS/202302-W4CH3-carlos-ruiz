1d01595912a59bd88f40364ab7091a849f0c46d9
CarlosRuiz <carua97@gmail.com>
2023-02-08 17:56:37 +0100

    Initial commit

---
+++ src/core/components/app/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { UserForm } from "../user-form/user-form";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Formulario</p>
+      <UserForm></UserForm>
+    </div>
+  );
+}
+
+export default App;
